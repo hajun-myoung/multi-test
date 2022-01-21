a69bd21c503656caef81831ca48db34b3e1e47c2
hajun <audgkwns44@gmail.com>
2022-01-22 00:38:22 +0900

    Update style.css

--- lineStyle.js
+++ lineStyle.js
@@ -1,13 +1,15 @@
-function color(){
-    const EVEN_COLOR = '#6DB1BF';
-    const ODD_COLOR = '#FFEAEC';
-    
-    let elements = document.querySelectorAll('body > main > article.question > div');
-    Object.entries(elements).forEach((item,i)=>{
-        item[1].style.backgroundColor = i%2==0 ? EVEN_COLOR : ODD_COLOR;
-    })
-    
-    console.log('Done : Coloring');
+function color() {
+  const EVEN_COLOR = "#6DB1BF";
+  const ODD_COLOR = "#FFEAEC";
+
+  let elements = document.querySelectorAll(
+    "body > main > article.question > div"
+  );
+  Object.entries(elements).forEach((item, i) => {
+    item[1].style.backgroundColor = i % 2 == 0 ? EVEN_COLOR : ODD_COLOR;
+  });
+
+  console.log("Done : Coloring");
 }
 
-window.addEventListener('DOMContentLoaded', color);
+window.addEventListener("DOMContentLoaded", color);
