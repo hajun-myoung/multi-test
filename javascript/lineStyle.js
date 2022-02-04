7bf519c8becc59a42170bdfbbbd6d5ec41936514
hajun <audgkwns44@gmail.com>
2022-02-04 14:17:27 +0900

    add advertice section

--- javascript/lineStyle.js
+++ javascript/lineStyle.js
@@ -2,9 +2,7 @@ function color() {
   const EVEN_COLOR = "#6DB1BF";
   const ODD_COLOR = "#FFEAEC";
 
-  let elements = document.querySelectorAll(
-    "body > main > article#question > ul > li > div"
-  );
+  let elements = document.querySelectorAll("article#question > ul > li > div");
   Object.entries(elements).forEach((item, i) => {
     item[1].style.backgroundColor = i % 2 == 0 ? EVEN_COLOR : ODD_COLOR;
   });
